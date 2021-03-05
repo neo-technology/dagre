887b0e65cdf3d4bf6f198fc10d68c23098f24c9e
neo-technology-build-agent <neo-technology-build-agent@neo4j.com>
2021-03-05 08:16:53 +0000

    Build-Agent: set version to 0.8.10

--- dist/dagre.js
+++ dist/dagre.js
@@ -2959,7 +2959,7 @@ function notime(name, fn) {
 }
 
 },{"./graphlib":7,"./lodash":10}],30:[function(require,module,exports){
-module.exports = "0.8.9";
+module.exports = "0.8.10";
 
 },{}],31:[function(require,module,exports){
 /**
